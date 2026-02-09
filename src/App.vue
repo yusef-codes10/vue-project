a996350f6137edc36186f026e89c29dca1c05290
Youcef <youcefcopy@gmail.com>
2026-02-09 12:53:29 +0100

    HomeCards component has been added

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <script setup>
 import JNavbar from './components/JNavbar.vue'
 import HeroSection from './components/HeroSection.vue'
+import HomeCards from './components/HomeCards.vue'
 </script>
 
 <template>
   <!-- <h1 class="text-2xl bg-sky-50">You did it!</h1> -->
   <JNavbar />
   <HeroSection title="Test Title" subTitle="Do not Scroll Down" />
+  <HomeCards />
 </template>
 
 <style scoped></style>

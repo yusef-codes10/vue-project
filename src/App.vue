a95137e3ae9a880d07e18ac1283c2eb8d11f0fb1
Youcef <youcefcopy@gmail.com>
2026-02-09 11:21:23 +0100

    creating the navbar component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,10 @@
-<script setup></script>
+<script setup>
+import JNavbar from './components/JNavbar.vue'
+</script>
 
 <template>
   <h1 class="text-2xl bg-sky-50">You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <JNavbar />
 </template>
 
 <style scoped></style>

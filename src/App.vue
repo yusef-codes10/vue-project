504255c55bdfa5051c07a1cd87ee22f17e222763
Youcef <youcefcopy@gmail.com>
2026-02-09 11:13:35 +0100

    tailwind is working

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup></script>
 
 <template>
-  <h1>You did it!</h1>
+  <h1 class="text-2xl bg-sky-50">You did it!</h1>
   <p>
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation

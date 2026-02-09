c2c3a6ac29b5f98422d6725612fa42a512a72e57
Youcef <youcefcopy@gmail.com>
2026-02-09 12:46:32 +0100

    using the props just for demonstration purposes

--- src/App.vue
+++ src/App.vue
@@ -7,8 +7,6 @@ import HeroSection from './components/HeroSection.vue'
   <!-- <h1 class="text-2xl bg-sky-50">You did it!</h1> -->
   <JNavbar />
   <HeroSection title="Test Title" subTitle="Do not Scroll Down" />
-  <HeroSection title="Jake the Dog" />
-  <HeroSection title="Become a Football Player" />
 </template>
 
 <style scoped></style>

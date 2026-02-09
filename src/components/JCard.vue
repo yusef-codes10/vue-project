1fe1bf330b277df2fd3a0b5ea1d5f19354e46f2f
Youcef <youcefcopy@gmail.com>
2026-02-09 13:25:19 +0100

    differnet way of binding classes

--- src/components/JCard.vue
+++ src/components/JCard.vue
@@ -1,8 +1,17 @@
-<script setup></script>
+<script setup>
+import { defineProps } from 'vue'
+
+defineProps({
+  bg: {
+    type: String,
+    default: 'bg-gray-100',
+  },
+})
+</script>
 
 <template>
-  <div class="bg-gray-100 p-6 rounded-lg shadow-md">
-    <slot></slot>
+  <div v-bind:class="`${bg} p-6 rounded-lg shadow-md`">
+    <!-- <slot></slot> -->
   </div>
 </template>
 

cc0c105be945f8e32f3c75549a4463d681109774
Youcef <youcefcopy@gmail.com>
2026-02-09 12:32:32 +0100

    the hero section component has been added

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
 import JNavbar from './components/JNavbar.vue'
+import HeroSection from './components/HeroSection.vue'
 </script>
 
 <template>
-  <h1 class="text-2xl bg-sky-50">You did it!</h1>
+  <!-- <h1 class="text-2xl bg-sky-50">You did it!</h1> -->
   <JNavbar />
+  <HeroSection />
 </template>
 
 <style scoped></style>

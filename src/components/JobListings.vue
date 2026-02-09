c74645bb441043364e57e273d10eb49c7d710fa8
Youcef <youcefcopy@gmail.com>
2026-02-09 15:52:41 +0100

    using props in the jobListing bcz it's external component to its parent and passing that as a prop object via binding

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -1,13 +1,21 @@
 <script setup>
 import jobData from '@/jobs.json'
 import { ref } from 'vue'
+import JobListing from './JobListing.vue'
 
 const jobs = ref(jobData)
 console.log(jobs.value)
 </script>
 
 <template>
-  <div v-for="job in jobs" :key="job.id"></div>
+  <section class="bg-blue-50 px-4 py-10">
+    <div class="container-xl lg:container m-auto">
+      <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">Browse Jobs</h2>
+      <div class="grid grid-col-1 md:grid-cols-3 gap-6">
+        <JobListing v-for="job in jobs" :key="job.id" v-bind:job="job" />
+      </div>
+    </div>
+  </section>
 </template>
 
 <style scoped></style>

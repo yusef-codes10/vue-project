9a120b995fbc78e84c55890a29627da7c3a3fe4c
Youcef <youcefcopy@gmail.com>
2026-02-10 12:17:41 +0100

    url switched | later when covering routes in vue

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -31,7 +31,7 @@ defineProps({
 
   <section v-if="showButton" class="m-auto max-w-lg my-10 px-6">
     <a
-      href="jobs.html"
+      href="/jobs"
       class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
       >View All Jobs</a
     >

e314b5f2799947e8697f4262af0f2273d5ef0ea3
Youcef <youcefcopy@gmail.com>
2026-02-10 12:10:27 +0100

    showButton props with a condition

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -8,6 +8,10 @@ console.log(jobs.value)
 
 defineProps({
   limit: Number,
+  showButton: {
+    type: Boolean,
+    default: true,
+  },
 })
 </script>
 
@@ -23,6 +27,14 @@ defineProps({
         />
       </div>
     </div>
+  </section>
+
+  <section class="m-auto max-w-lg my-10 px-6">
+    <a
+      href="jobs.html"
+      class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
+      >View All Jobs</a
+    >
   </section>
 </template>
 
